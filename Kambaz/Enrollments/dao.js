644396c336b06aa142e7d866d6864f95f816d75d
Ella <pitts.ell@northeastern.edu>
2025-11-21 00:15:47 -0500

    Enroll and Unenroll major bug fixed

--- Kambaz/Enrollments/dao.js
+++ Kambaz/Enrollments/dao.js
@@ -8,6 +8,21 @@ export default function EnrollmentsDao(db) {
     enrollments.push({ _id: uuidv4(), user: userId, course: courseId });
   }
 
-  return { enrollUserInCourse };
+    function unenrollUserFromCourse(userId, courseId) {
+    const { enrollments } = db;
+    const index = enrollments.findIndex(
+      (enrollment) => enrollment.user === userId && enrollment.course === courseId
+    );
+    if (index !== -1) {
+      enrollments.splice(index, 1);
+    }
+  }
+
+  function findEnrollmentsForUser(userId) {
+    const { enrollments } = db;
+    return enrollments.filter((enrollment) => enrollment.user === userId);
+  }
+
+  return { enrollUserInCourse, unenrollUserFromCourse, findEnrollmentsForUser };
 
 }
--- Kambaz/Users/routes.js
+++ Kambaz/Users/routes.js
@@ -94,11 +94,18 @@ export default function UserRoutes(app, db) {
     res.sendStatus(200);
   };
 
+  const findEnrollmentsForUser = (req, res) => {
+    let { userId } = req.params;
+    if (userId === "current") {
+      const currentUser = req.session["currentUser"];
+      userId = currentUser._id;
+    }
+    const enrollments = enrollmentsDao.findEnrollmentsForUser(userId);
+    res.json(enrollments);
+  };
+
   app.post("/api/users", createUser);
   app.get("/api/users", findAllUsers);
-  // app.post("/api/users/:userId/courses/:courseId", enrollUserInCourse);
-  // app.delete("/api/users/:userId/courses/:courseId", unenrollUserFromCourse);
-  // General routes come after specific ones
   app.get("/api/users/:userId", findUserById);
   app.put("/api/users/:userId", updateUser);
   app.delete("/api/users/:userId", deleteUser);
@@ -106,9 +113,88 @@ export default function UserRoutes(app, db) {
   app.post("/api/users/signin", signin);
   app.post("/api/users/signout", signout);
   app.post("/api/users/profile", profile);
-
-  // // Enroll and unenroll from course to dashbaord 
   app.post("/api/users/:userId/courses/:courseId", enrollUserInCourse);
   app.delete("/api/users/:userId/courses/:courseId", unenrollUserFromCourse);
-
+  app.get("/api/users/:userId/enrollments", findEnrollmentsForUser);
 }
+
+// import UsersDao from "./dao.js";
+
+// export default function UserRoutes(app, db) {
+//   const dao = UsersDao(db);
+
+//   const createUser = (req, res) => {
+//     const user = dao.createUser(req.body);
+//     res.json(user);
+//   };
+
+//   const deleteUser = (req, res) => {
+//     dao.deleteUser(req.params.userId);
+//     res.sendStatus(200);
+//   };
+
+//   const findAllUsers = (req, res) => {
+//     const users = dao.findAllUsers();
+//     res.json(users);
+//   };
+
+//   const findUserById = (req, res) => {
+//     const user = dao.findUserById(req.params.userId);
+//     res.json(user);
+//   };
+
+//   const updateUser = (req, res) => {
+//     const userId = req.params.userId;
+//     const userUpdates = req.body;
+//     dao.updateUser(userId, userUpdates);
+//     const currentUser = dao.findUserById(userId);
+//     req.session["currentUser"] = currentUser;
+//     res.json(currentUser);
+//   };
+
+//   const signup = (req, res) => {
+//     const user = dao.findUserByUsername(req.body.username);
+//     if (user) {
+//       res.status(400).json({ message: "Username already in use" });
+//       return;
+//     }
+//     const currentUser = dao.createUser(req.body);
+//     req.session["currentUser"] = currentUser;
+//     res.json(currentUser);
+//   };
+
+//   const signin = (req, res) => {
+//     const { username, password } = req.body;
+//     const currentUser = dao.findUserByCredentials(username, password);
+//     if (currentUser) {
+//       req.session["currentUser"] = currentUser;
+//       res.json(currentUser);
+//     } else {
+//       res.status(401).json({ message: "Unable to login. Try again later." });
+//     }
+//   };
+
+//   const signout = (req, res) => {
+//     req.session.destroy();
+//     res.sendStatus(200);
+//   };
+
+//   const profile = (req, res) => {
+//     const currentUser = req.session["currentUser"];
+//     if (!currentUser) {
+//       res.sendStatus(401);
+//       return;
+//     }
+//     res.json(currentUser);
+//   };
+
+//   app.post("/api/users", createUser);
+//   app.get("/api/users", findAllUsers);
+//   app.get("/api/users/:userId", findUserById);
+//   app.put("/api/users/:userId", updateUser);
+//   app.delete("/api/users/:userId", deleteUser);
+//   app.post("/api/users/signup", signup);
+//   app.post("/api/users/signin", signin);
+//   app.post("/api/users/signout", signout);
+//   app.post("/api/users/profile", profile);
+// }
