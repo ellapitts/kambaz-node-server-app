96ffc55de6a32a4aae86550c6210c73bd891d518
Ella <pitts.ell@northeastern.edu>
2025-12-02 17:49:24 -0500

    CORS config bugs

--- index.js
+++ index.js
@@ -20,38 +20,38 @@ mongoose.connect(CONNECTION_STRING)  // Connects to kambaz database
 const app = express();
 // The deployed frontend URL is read directly from the environment variable
 const CLIENT_URL = process.env.CLIENT_URL;
+app.use(express.json());
 
 app.use(
   cors({
     credentials: true,
+    origin: function(origin, callback) {
+      const allowedOrigins = [
     // origin: function(origin, callback) {
-      origin: CLIENT_URL,
+      CLIENT_URL,
+      'https://kambaz-next-js-main.vercel.app',
+      'https://kambaz-next-js-main-git-main-ellapitts-projects.vercel.app',
+      'https://kambaz-next-js-main2-git-a6-ellapitts-projects.vercel.app',
+      'https://kambaz-next-js-main2-9cpyoxzsu-ellapitts-projects.vercel.app',
+      // for local dev
+         'http://localhost:3000',
+    'http://localhost:3001',
+    'http://localhost:3002',
+    'http://127.0.0.1:3000',
+    'http://127.0.0.1:3001',
+    'http://127.0.0.1:3002',
+    ];
+          //Allow any Vercel preview deployment
+     const vercelPattern = /^https:\/\/kambaz-next-js-main2.*\.vercel\.app$/;
+      
+      if (!origin || allowedOrigins.includes(origin) || vercelPattern.test(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    }
   })
 );
-    //   [
-    //     process.env.CLIENT_URL,
-    // 'https://kambaz-next-js-main.vercel.app',
-    // 'https://kambaz-next-js-main-git-main-ellapitts-projects.vercel.app',
-    // 'https://kambaz-next-js-main2-git-a6-ellapitts-projects.vercel.app',
-    // 'http://localhost:3000',
-    // 'http://localhost:3001',
-    // 'http://localhost:3002',
-    // 'http://127.0.0.1:3000',
-    // 'http://127.0.0.1:3001',
-    // 'http://127.0.0.1:3002'
-    //   ];
-
-      // Allow any Vercel preview deployment
-//       const vercelPattern = /^https:\/\/kambaz-next-js-main2.*\.vercel\.app$/;
-      
-//       if (!origin || allowedOrigins.includes(origin) || vercelPattern.test(origin)) {
-//         callback(null, true);
-//       } else {
-//         callback(new Error('Not allowed by CORS'));
-//       }
-//     }
-//   })
-// );
 
 const sessionOptions = {
   secret: process.env.SESSION_SECRET || "kambaz",
@@ -67,7 +67,6 @@ if (process.env.SERVER_ENV !== "development") {
   };
 }
 app.use(session(sessionOptions));
-app.use(express.json());
 
 // Routes loaded
 Hello(app); // pass app reference to Hello
